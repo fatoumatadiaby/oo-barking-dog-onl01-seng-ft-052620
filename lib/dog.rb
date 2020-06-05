fe214ffd904acbb2acae143a3d9e7c53aeefb78c
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-05 00:56:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,3 @@
 class Dog 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
- 
-  def name
-    @this_dogs_name
-  end
-end
- 
-lassie = Dog.new
-lassie.name = "fido"
- 
-puts fido.name
+
+end 
